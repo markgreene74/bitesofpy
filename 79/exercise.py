9a3a13c5ab218d5cfb3457c49888d356ca26dd6e
markgreene74 <markgreene74@users.noreply.github.com>
2019-09-01 19:40:01 +0100

    PyBites exercise #79

---
+++ 79/exercise.py
@@ -0,0 +1,24 @@
+import csv
+import requests
+from collections import defaultdict
+
+CSV_URL = "https://bit.ly/2HiD2i8"
+
+
+def get_csv():
+    """Use requests to download the csv and return the
+       decoded content"""
+    data = requests.get(CSV_URL)
+    return csv.DictReader(data.text.splitlines())
+
+
+def create_user_bar_chart(content):
+    """Receives csv file (decoded) content and returns a table of timezones
+       and their corresponding member counts in pluses (see Bite/tests)"""
+    table = defaultdict(int)
+    # content is a csv.DictReader
+    for entry in content:
+        table[entry["tz"]] += 1
+    table_sorted = sorted(table.items(), key=lambda kv: kv[0])
+    for i in table_sorted:
+        print(f'{i[0].ljust(20)} | {"+" * i[1]}')
