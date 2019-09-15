53691806183102c2ae59b270fbc451de30a857c9
markgreene74 <markgreene74@users.noreply.github.com>
2019-09-15 12:52:43 +0100

    PyBites exercise #95

---
+++ 95/exercise.py
@@ -0,0 +1,17 @@
+from datetime import date
+
+MSG = "Hey {}, there are more people with your birthday!"
+
+
+class BirthdayDict(dict):
+    """Override dict to print a message every time a new person is added that has
+       the same birthday (day+month) as somebody already in the dict"""
+
+    def __init__(self, *args, **kwargs):
+        self.update(*args, **kwargs)
+
+    def __setitem__(self, name, birthday):
+        for i in self.values():
+            if birthday.month == i.month and birthday.day == i.day:
+                print(MSG.format(name))
+        dict.__setitem__(self, name, birthday)
