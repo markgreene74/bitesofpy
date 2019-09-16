c7384f836ab1129f52930af660e130cd9df76ab4
markgreene74 <markgreene74@users.noreply.github.com>
2019-09-16 22:13:24 +0100

    PyBites exercise #111

---
+++ 111/exercise.py
@@ -0,0 +1,10 @@
+import requests
+
+IPINFO_URL = "http://ipinfo.io/{ip}/json"
+
+
+def get_ip_country(ip_address):
+    """Receives ip address string, use IPINFO_URL to get geo data,
+       parse the json response returning the country code of the IP"""
+    data = requests.get(IPINFO_URL.format(ip=ip_address)).json()
+    return data["country"]
