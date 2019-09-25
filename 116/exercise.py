c9c46e6e21df2f4310ba5facc514cd843657cb0b
markgreene74 <markgreene74@users.noreply.github.com>
2019-09-25 22:18:24 +0100

    Update exercise.py

--- 116/exercise.py
+++ 116/exercise.py
@@ -5,5 +5,5 @@ ONE_KB = 1024
 
 def get_files(dirname, size_in_kb):
     """Return files in dirname that are >= size_in_kb"""
-    ls = os.listdir()
-    return [file for file in ls if os.stat(file).st_size >= size_in_kb * ONE_KB]
+    ls = os.listdir(dirname)
+    return [file for file in ls if os.stat(os.path.join(dirname, file)).st_size >= size_in_kb * ONE_KB]
