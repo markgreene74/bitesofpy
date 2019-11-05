3481beb7acaf0db623fbd78dc84a512dfe77b57f
markgreene74 <markgreene74@users.noreply.github.com>
2019-11-05 21:35:41 +0000

    PyBites exercise #210

---
+++ 210/exercise.py
@@ -0,0 +1,15 @@
+from typing import List
+
+Numbers = List[int]
+
+
+def sum_numbers(numbers: Numbers) -> int:
+    """Sums numbers
+
+    :param numbers: a list of numbers
+    :type numbers: list
+    :raises TypeError: if not all numeric values passed in
+    :return: sum of numbers
+    :rtype: int
+    """
+    pass
