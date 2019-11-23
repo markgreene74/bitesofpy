f0770ce653a4b9264692410a2dce8253669be101
markgreene74 <markgreene74@users.noreply.github.com>
2019-11-23 21:00:51 +0000

    PyBites exercise #224

---
+++ 224/exercise.py
@@ -0,0 +1,8 @@
+# https://codechalleng.es/bites/224/
+import re
+
+def get_sentences(text):
+    """Return a list of sentences as extracted from the text passed in.
+       A sentence starts with [A-Z] and ends with [.?!]"""
+    found = re.findall(r"([A-Z].+?[.?!])\s(?=[A-Z]|$)", text, re.DOTALL)
+    return [s.replace("\n", " ") for s in found]
