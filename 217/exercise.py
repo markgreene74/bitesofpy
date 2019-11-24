ec730ba1f950a230d824711d096c58a39b845a83
markgreene74 <markgreene74@users.noreply.github.com>
2019-11-24 14:22:33 +0000

    PyBites exercise #217

---
+++ 217/exercise.py
@@ -0,0 +1,17 @@
+# https://codechalleng.es/bites/217/
+from contextlib import redirect_stdout
+from io import StringIO
+from types import BuiltinFunctionType
+
+
+def get_len_help_text(builtin: BuiltinFunctionType) -> int:
+    """Receives a builtin, and returns the length of its help text.
+       You need to redirect stdout from the help builtin.
+       If the the object passed in is not a builtin, raise a ValueError.
+    """
+    if not isinstance(builtin, BuiltinFunctionType):
+        raise ValueError("Not a builtin function")
+    f = StringIO()
+    with redirect_stdout(f):
+        help(builtin)
+    return len(f.getvalue())
