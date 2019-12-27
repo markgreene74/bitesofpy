f78932b8d0a37a3635113d3cb3e5aa104e2c464e
markgreene74 <markgreene74@users.noreply.github.com>
2019-12-27 14:49:10 +0000

    PyBites exercise #166

---
+++ 166/exercise.py
@@ -0,0 +1,35 @@
+# https://codechalleng.es/bites/166/
+# https://docs.python.org/3/library/configparser.html
+import configparser
+
+
+class ToxIniParser:
+    def __init__(self, ini_file):
+        """Use configparser to load ini_file into self.config"""
+        _config = configparser.ConfigParser()
+        _config.read(ini_file)
+        self.config = _config
+
+    @property
+    def number_of_sections(self):
+        """Return the number of sections in the ini file.
+           New to properties? -> https://pybit.es/property-decorator.html
+        """
+        return len(self.config.sections())
+
+    @property
+    def environments(self):
+        """Return a list of environments
+           (= "envlist" attribute of [tox] section)"""
+        _env_str = self.config["tox"]["envlist"].replace(",", " ")
+        return [i.strip() for i in _env_str.split()]
+
+    @property
+    def base_python_versions(self):
+        """Return a list of all basepython across the ini file"""
+        return set(
+            self.config[i][j]
+            for i in self.config.sections()
+            for j in self.config[i]
+            if j == "basepython"
+        )
