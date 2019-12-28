66f4a627179519597e585c2e717ae60ad02dfd9e
markgreene74 <markgreene74@users.noreply.github.com>
2019-12-28 19:56:55 +0000

    PyBites exercise #199

---
+++ 199/exercise.py
@@ -0,0 +1,54 @@
+# https://codechalleng.es/bites/199/
+# see __mro__ output in Bite description
+
+
+class Person:
+    def __init__(self):
+        self.identity = "person"
+
+    def __str__(self):
+        _str = f"I am a {self.identity}"
+        return _str
+
+
+class Mother(Person):
+    def __init__(self):
+        self.secondary_identity = "awesome mom"
+        super().__init__()
+
+    def __str__(self):
+        _inherited = super().__str__()
+        _str = _inherited + " and " + self.secondary_identity
+        return _str
+
+
+class Father(Person):
+    def __init__(self):
+        self.secondary_identity = "cool daddy"
+        super().__init__()
+
+    def __str__(self):
+        _inherited = super().__str__()
+        _str = _inherited + " and " + self.secondary_identity
+        return _str
+
+
+class Child(Father, Mother):
+    def __init__(self):
+        self.new_identity = "the coolest kid"
+        super().__init__()
+
+    def __str__(self):
+        _str = f"I am {self.new_identity}"
+        return _str
+
+# person = Person()
+# dad = Father()
+# mom = Mother()
+# child = Child()
+
+# print(person)
+# print(dad)
+# print(mom)
+# print(child)
+# print(Child.__mro__)
