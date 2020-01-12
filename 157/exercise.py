7f22090dd40822d1cbe5c6f7e9d5833cf5cbe2e0
markgreene74 <markgreene74@users.noreply.github.com>
2020-01-12 10:27:25 +0000

    PyBites exercise #157

---
+++ 157/exercise.py
@@ -0,0 +1,10 @@
+# https://codechalleng.es/bites/157/
+# https://docs.python.org/3/howto/unicode.html#comparing-strings
+import unicodedata
+
+
+def filter_accents(text):
+    """Return a sequence of accented characters found in
+       the passed in lowercased text string
+    """
+    return sorted(set(i.lower() for i in text if "WITH" in unicodedata.name(i)))
