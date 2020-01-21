982c98f26c653d4e3c48634c89c4e5183d65a6aa
markgreene74 <markgreene74@users.noreply.github.com>
2020-01-21 22:08:49 +0000

    PyBites exercise #131

---
+++ 131/exercise.py
@@ -0,0 +1,44 @@
+# https://codechalleng.es/bites/131/
+import re
+from collections import defaultdict
+
+
+output = """
+                                       mohh@SERENiTY
+ MMMMMMMMMMMMMMMMMMMMMMMMMmds+.        OS: Mint 19 tara
+ MMm----::-://////////////oymNMd+'     Kernel: x86_64 Linux 4.15.0-34-generic
+ MMd      /++                -sNMd:    Uptime: 1d 4m
+ MMNso/'  dMM    '.::-. .-::.' .hMN:   Packages: 2351
+ ddddMMh  dMM   :hNMNMNhNMNMNh: 'NMm   Shell: zsh 5.4.2
+     NMm  dMM  .NMN/-+MMM+-/NMN' dMM   Resolution: 1366x768
+     NMm  dMM  -MMm  'MMM   dMM. dMM   DE: Cinnamon 3.8.9
+     NMm  dMM  -MMm  'MMM   dMM. dMM   WM: Muffin
+     NMm  dMM  .mmd  'mmm   yMM. dMM   WM Theme: Linux Mint (Mint-Y)
+     NMm  dMM'  ..'   ...   ydm. dMM   GTK Theme: Mint-Y [GTK2/3]
+     hMM- +MMd/-------...-:sdds  dMM   Icon Theme: Mint-Y
+     -NMm- :hNMNNNmdddddddddy/'  dMM   Font: Noto Sans 9
+      -dMNs-''-::::-------.''    dMM   CPU: AMD A10-7400P Radeon R6, 10 Compute Cores 4C+6G @ 4x 2.5GHz [101.0°C]
+       '/dMNmy+/:-------------:/yMMM   GPU: AMD KAVERI (DRM 2.50.0 / 4.15.0-34-generic, LLVM 6.0.0)
+          ./ydNMMMMMMMMMMMMMMMMMMMMM   RAM: 1886MiB / 6915MiB
+             \.MMMMMMMMMMMMMMMMMMM    
+"""
+
+
+def sysinfo_scrape(output):
+    """Scrapes the output from screenfetch and returns a dictionary"""
+    result = defaultdict(str)
+    all_str = re.findall(r"^.*\s{3}(\w.*)$", output, re.MULTILINE)
+    for i in all_str:
+        if ":" not in i:
+            # this is the name
+            result["Name"] = i
+        else:
+            _split = i.split(": ")
+            result[_split[0]] = _split[1]
+    return result
+
+
+'''
+50 out of 50 users completed this Bite.
+Resolution time: ~44 min. (avg. submissions of 5-240 min.) - awesome, you solved it in 20 min. 💪
+'''
