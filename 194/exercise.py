d54ab3e75e0468d401a1f21814875219a79256af
markgreene74 <markgreene74@users.noreply.github.com>
2020-02-03 21:34:38 +0000

    PyBites exercise #194

---
+++ 194/exercise.py
@@ -0,0 +1,14 @@
+# https://codechalleng.es/bites/194/
+from functools import lru_cache
+
+
+@lru_cache(maxsize=None)
+def cached_fib(n):
+    if n < 2:
+        return n
+    return cached_fib(n - 1) + cached_fib(n - 2)
+
+
+'''
+Resolution time: ~25 min. (avg. submissions of 5-240 min.) - awesome, you solved it in 10 min. 💪
+'''
