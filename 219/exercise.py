4faf26d4ae7d1cd5e18dbcfee3baee565223e14a
markgreene74 <markgreene74@users.noreply.github.com>
2020-02-04 21:30:39 +0000

    Update exercise.py

--- 219/exercise.py
+++ 219/exercise.py
@@ -10,3 +10,7 @@ def gen_bite_planning(num_bites=1, num_days=1, start_date=TODAY):
         result_date += timedelta(days=num_days)
         for i in range(num_bites):
             yield result_date
+
+'''
+Resolution time: ~40 min. (avg. submissions of 5-240 min.) - awesome, you solved it in 15 min. 💪
+'''
