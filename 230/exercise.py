0f2a41624cb4121ec6d3296526ea3d146c52094b
markgreene74 <markgreene74@users.noreply.github.com>
2020-02-10 22:27:32 +0000

    PyBites exercise #230

---
+++ 230/exercise.py
@@ -0,0 +1,30 @@
+# https://codechalleng.es/bites/230/
+
+# https://docs.python.org/3/reference/datamodel.html#special-method-names
+# https://github.com/markgreene74/bitesofpy/blob/master/31/exercise.py
+
+THUMBS_UP, THUMBS_DOWN = "👍", "👎"
+
+
+class Thumbs:
+    # def __init__(self):
+    #     pass
+
+    def __mul__(self, other):
+        if other == 0:
+            raise ValueError("Specify a number")
+
+        thumb = THUMBS_UP if other > 0 else THUMBS_DOWN
+        n_thumbs = abs(other)
+
+        if n_thumbs > 3:
+            return f"{thumb} ({str(n_thumbs)}x)"
+        else:
+            return f"{thumb * n_thumbs}"
+
+    def __rmul__(self, other):
+        return self.__mul__(other)
+
+'''
+Resolution time: ~40 min. (avg. submissions of 5-240 min.) - awesome, you solved it in 21 min. 💪
+'''
