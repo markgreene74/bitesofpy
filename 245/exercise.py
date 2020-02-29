75c4db6a4ff0a814d457db4164c046e02b0e4b5a
markgreene74 <markgreene74@users.noreply.github.com>
2020-02-29 13:45:37 +0000

    PyBites exercise #245

---
+++ 245/exercise.py
@@ -0,0 +1,18 @@
+# https://codechalleng.es/bites/245/
+STAR = "+"
+LEAF = "*"
+TRUNK = "|"
+
+
+def generate_improved_xmas_tree(rows=10):
+    """Generate a xmas tree with a star (+), leafs (*) and a trunk (|)
+       for given rows of leafs (default 10).
+       For more information see the test and the bite description"""
+    tree = f'{" " * (rows - 1)}{STAR}'
+    for i in range(rows + 1):
+        tree += f'{" " * (rows - i)}{LEAF * (i * 2 - 1)}'.rstrip() + "\n"
+    _max_leafs = rows * 2 - 1
+    _trunk = int(round(_max_leafs / 2) + 1)
+    tree += f'{" " * int(_max_leafs / 4)}{TRUNK * _trunk}\n'
+    tree += f'{" " * int(_max_leafs / 4)}{TRUNK * _trunk}\n'
+    return tree.strip("\n")
