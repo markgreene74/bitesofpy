205910407d1795a81cd2fa983d4ba0c9c6a4df90
markgreene74 <markgreene74@users.noreply.github.com>
2020-04-26 18:48:20 +0100

    PyBites exercise #270

---
+++ 270/exercise.py
@@ -0,0 +1,13 @@
+# https://codechalleng.es/bites/270/
+from collections import Counter
+
+
+def freq_digit(num: int) -> int:
+    c = Counter(str(num))
+    n, freq = c.most_common(1)[0]
+    return int(n)
+
+
+'''
+Resolution time: ~30 min. (avg. submissions of 5-240 min.) - awesome, you solved it in 7 min. 💪
+'''
