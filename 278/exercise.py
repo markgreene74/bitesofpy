68f73d9d3f61b66a44953e0fed99c233b120d256
markgreene74 <markgreene74@users.noreply.github.com>
2020-07-26 19:09:26 +0100

    PyBites exercise #278

---
+++ 278/exercise.py
@@ -0,0 +1,18 @@
+# https://codechalleng.es/bites/278/
+from collections import Counter
+
+
+def major_n_minor(numbers):
+    """
+    Input: an array with integer numbers
+    Output: the majority and minority number
+    """
+
+    c = Counter(numbers)
+    major, _ = c.most_common()[0]
+    minor, _ = c.most_common()[-1]
+    return major, minor
+
+'''
+Resolution time: ~22 min. (avg. submissions of 5-240 min.) - awesome, you solved it in 9 min. 💪
+'''
