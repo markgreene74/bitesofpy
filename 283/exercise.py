dbc6c6d6dc9a8825f311dd533d979243ccaf11bc
markgreene74 <markgreene74@users.noreply.github.com>
2020-08-01 16:53:09 +0100

    PyBites exercise #283

---
+++ 283/exercise.py
@@ -0,0 +1,13 @@
+# https://codechalleng.es/bites/283/
+import datetime
+
+
+def tomorrow(someday=None):
+    # Your code goes here
+    today = someday or datetime.date.today()
+    one_day = datetime.timedelta(days=1)
+    return today + one_day
+
+'''
+Resolution time: ~26 min. (avg. submissions of 5-240 min.) - awesome, you solved it in 9 min. 💪
+'''
